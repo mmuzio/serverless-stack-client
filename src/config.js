e9c5565c3d75ab9d58f76463dcb3df94f05ae8d1
mmuzio <mmuzio@hmc.edu>
2020-10-26 11:22:58 -0400

    setup react app

---
+++ src/config.js
@@ -0,0 +1,15 @@
+export default {
+    s3: {
+        REGION: "us-east-1",
+        BUCKET: "notes-app-gitdate"
+    },
+    apiGateway: {
+        REGION: "us-east-1",
+        URL: "https://dp9bskkrna.execute-api.us-east-1.amazonaws.com/prod"
+    },
+    cognito: {
+        REGION: "us-east-1",
+        USER_POOL_ID: "us-east-1_DG8vwjPSX",
+        APP_CLIENT_ID: "74p0nv37jishf3o3q23hld5veu",
+        IDENTITY_POOL_ID: "us-east-1:c89c93d7-a56c-4f8c-a500-057a67c13275"
+} };
